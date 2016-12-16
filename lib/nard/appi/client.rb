c9f6463c2577c7b1544eff853b1f206489c0f5e9
Shu Fujita <s.fujita@nard.tech>
2016-12-16 22:34:02 +0900

    20161216_223400 Update - [Lib] Nard::Appi::Client#initialize

--- lib/nard/appi/client.rb
+++ lib/nard/appi/client.rb
@@ -24,14 +24,15 @@ module Nard
         end
       end
 
-      def initialize( gem_top_namespace, options = {} )
-        options_keys = gem_top_namespace::OPTIONS_KEYS
+      def initialize( options = {} )
+        _top_namespace = self.class.top_namespace
+        options_keys = _top_namespace::OPTIONS_KEYS
 
         self.class.instance_eval do
           set_attr_accessors( options_keys )
         end
 
-        options = gem_top_namespace.options.merge( options ).with_indifferent_access
+        options = _top_namespace.options.merge( options ).with_indifferent_access
 
         options_keys.each do | key |
           send( "#{ key }=", options[ key ] )
