c52c3a8f9abba087283215c09383d762a86f637a
Shu Fujita <s.fujita@nard.tech>
2016-12-17 00:00:53 +0900

    20161217_000900 Refactor - [Lib] Nard::Appi::Client

--- lib/nard/appi/client.rb
+++ lib/nard/appi/client.rb
@@ -9,51 +9,92 @@ module Nard
 
       include Nard::Appi::ClientExt::Request
 
+      # @!group Methods for DSL
+
       def self.client_for( namespace )
-        @top_namespace = namespace
+        set_top_namespace( namespace )
       end
+
+      def self.concerns( *namespaces, under: nil )
+        raise ArgumentError unless under.present?
+        namespaces.each do | namespace |
+          include_concerned_module( namespace, under )
+        end
+      end
+
+      # @!endgroup
 
       class << self
-        attr_reader :top_namespace
-      end
 
-      def self.concerns( *namespaaces, under: nil )
-        raise ArgumentError unless under.present?
-        namespaaces.each do | file_basename |
-          require "#{ under }/#{ file_basename.to_s.downcase }"
-          module_name = ( under.classify.constantize ).const_get( file_basename.to_s.capitalize )
-          puts "#{ module_name } is included."
+        include ActiveSupport::Callbacks
+
+        attr_reader :top_namespace
+
+        private
+
+        # @!group Private methods for DSL
+
+        def set_top_namespace( top_namespace )
+          @top_namespace = top_namespace
+        end
+
+        def include_concerned_module( namespace, under )
+          require "#{ under }/#{ namespace.to_s.downcase }"
+
+          module_name = under.classify
+                             .constantize
+                             .const_get( namespace.to_s.capitalize )
+
+          # puts "#{ module_name } is included."
           include module_name
         end
+
+        # @!endgroup
+
+        def set_attr_accessor( option_key )
+          getter_method = option_key.to_sym
+          setter_method = "#{ option_key }=".to_sym
+          writer = option_key.to_sym
+          attr_reader getter_method unless getter_method.in?( instance_methods )
+          attr_writer writer unless setter_method.in?( instance_methods )
+        end
+
       end
 
       def initialize( options = {} )
-        _top_namespace = self.class.top_namespace
-        options_keys = _top_namespace::OPTIONS_KEYS
+        set_attr_accessors
+        set_option_values( options )
+      end
+
+      private
+
+      def set_attr_accessors
+        top_namespace = self.class.top_namespace
+        options_keys = top_namespace::OPTIONS_KEYS
 
         self.class.instance_eval do
-          set_attr_accessors( options_keys )
-        end
+          return if @_attr_accessors_defined
 
-        options = _top_namespace.options.merge( options ).with_indifferent_access
+          options_keys.each do | option_key |
+            set_attr_accessor( option_key )
+          end
+
+          @_attr_accessors_defined = true
+        end
+      end
+
+      def set_option_values( options )
+        options_keys = self.class.top_namespace::OPTIONS_KEYS
+
+        options = self.class
+                      .top_namespace
+                      .options
+                      .merge( options )
+                      .with_indifferent_access
 
         options_keys.each do | key |
           send( "#{ key }=", options[ key ] )
         end
-      end
-
-      def self.set_attr_accessors( options_keys )
-        return if @_attr_accessors_defined
-
-        options_keys.each do | option_key |
-          getter_method = option_key.to_sym
-          setter_method = "#{ option_key }=".to_sym
-          writer = option_key.to_sym
-          attr_reader getter_method unless getter_method.in?( self.class.instance_methods )
-          attr_writer writer unless setter_method.in?( self.class.instance_methods )
-        end
-
-        @_attr_accessors_defined = true
       end
 
     end
